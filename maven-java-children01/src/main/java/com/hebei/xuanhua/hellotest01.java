a7b45ab4e39f8a3d59b53ba4888b24637e483324
lsbrand <lsbrand@hebei.com>
2023-07-15 09:09:13 +0800

    second java-project commit

--- maven-java-children01/src/main/java/com/hebei/xuanhua/hellotest01.java
+++ maven-java-children01/src/main/java/com/hebei/xuanhua/hellotest01.java
@@ -3,5 +3,6 @@ package com.hebei.xuanhua;
 public class hellotest01 {
     public static void main(String[] args) {
         System.out.println("hello-world");
+        System.out.println("hello-world002");
     }
 }
