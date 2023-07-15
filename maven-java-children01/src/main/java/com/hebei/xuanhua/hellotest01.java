c2d388bf151ed57b9199ba94cc5490caf3175403
lsslaa <95161952+lsslaa@users.noreply.github.com>
2023-07-15 09:56:14 +0800

    Update on github first hellotest01.java

--- maven-java-children01/src/main/java/com/hebei/xuanhua/hellotest01.java
+++ maven-java-children01/src/main/java/com/hebei/xuanhua/hellotest01.java
@@ -8,5 +8,6 @@ public class hellotest01 {
         System.out.println("hello-world-master01");
         System.out.println("hello-hot-fix01");
         System.out.println("想github的第一次提交");
+        System.out.println("在github上的第一次改动");
     }
 }
